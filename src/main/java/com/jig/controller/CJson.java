67126086c7314811a3c9b0e76c3fcb703093638b
YangChengxxyy <45156288+YangChengxxyy@users.noreply.github.com>
2020-01-18 14:37:24 +0800

    first version

--- src/main/java/com/jig/controller/CJson.java
+++ src/main/java/com/jig/controller/CJson.java
@@ -2,6 +2,7 @@ package com.jig.controller;
 
 import com.jig.entity.DemoEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,9 +11,22 @@ import java.util.List;
 
 @RestController
 public class CJson {
-    @RequestMapping("get_demo_list")
-    public List<DemoEntity> getDemoList(HttpServletRequest request){
+    @RequestMapping(value = "get_demo_list", method = {RequestMethod.POST, RequestMethod.GET})
+    public List<DemoEntity> getDemoList(HttpServletRequest request) {
         int pageNumber = Integer.parseInt(request.getParameter("page_number"));
-        return null;
+        List<DemoEntity> a = new ArrayList<>();
+        DemoEntity people = new DemoEntity();
+        people.setName("yc");
+        people.setSex("男");
+        people.setStu_no("189050536");
+        for (int i = 0; i < pageNumber; i++) {
+            a.add(people);
+        }
+        return a;
+    }
+
+    @RequestMapping("get_demo_page_number")
+    public int getDemoPageNumber() {
+        return 3;
     }
 }
--- src/main/java/com/jig/controller/CWeb.java
+++ src/main/java/com/jig/controller/CWeb.java
@@ -4,15 +4,22 @@ import com.jig.entity.DemoEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CWeb {
-    @RequestMapping("show_demo")
-    public String showDemo(Model model){
-        DemoEntity hxy = new DemoEntity();
+    @RequestMapping(value = "show_demo", method = {RequestMethod.POST, RequestMethod.GET})
+    public String showDemo(Model model) {
+        List<DemoEntity> a = new ArrayList<>();
+        DemoEntity people = new DemoEntity();
+        people.setName("yc");
+        people.setSex("男");
+        people.setStu_no("189050536");
+        a.add(people);
+        model.addAttribute("peoples", a);
         return "demo";
     }
 }
