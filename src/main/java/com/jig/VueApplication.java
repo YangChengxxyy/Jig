44f1418302a4158ac9feb32f038d472748b888b4
YangChengxxyy <853364106@qq.com>
2020-03-22 11:57:10 +0800

    添加Kotlin依赖

--- src/main/java/com/jig/VueApplication.java
+++ src/main/java/com/jig/VueApplication.java
@@ -1,13 +1,10 @@
 package com.jig;
 
-
-
 import com.jig.utils.SpringUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
 
 @EnableScheduling
 @SpringBootApplication
